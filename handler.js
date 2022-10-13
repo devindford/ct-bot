cd0a1e9d65426c7b69b24e7c8e62f10ed3d879ed
Devin Ford <devindford@outlook.com>
2022-10-12 20:57:03 -0400

    feat: start to build out bot

--- handler.js
+++ handler.js
@@ -1,4 +1,16 @@
 'use strict';
+require('dotenv').config()
+const { Client, GatewayIntentBits } = require('discord.js')
+const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+const token = process.env.DISCORD_TOKEN
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+// Login to Discord with your client's token
+client.login(token);
 
 module.exports.hello = async (event) => {
   return {
